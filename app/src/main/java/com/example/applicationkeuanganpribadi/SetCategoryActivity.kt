871f82599f87f53e142b228725723f54df66b524
kurniawan vendy wijayana <gilfordkerenz@gmail.com>
2020-10-12 22:53:25 +0800

    Home & Profile

--- app/src/main/java/com/example/applicationkeuanganpribadi/SetCategoryActivity.kt
+++ app/src/main/java/com/example/applicationkeuanganpribadi/SetCategoryActivity.kt
@@ -3,9 +3,9 @@ package com.example.applicationkeuanganpribadi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ContohActivity : AppCompatActivity() {
+class SetCategoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contoh)
+        setContentView(R.layout.activity_set_category)
     }
 }
